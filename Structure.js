4d578c6358b9d66d9d8662ce6044e725808d1cb1
michaelwty <wty1009@outlook.com>
2017-07-20 17:29:29 -0700

    Update Structure.js

--- Structure.js
+++ Structure.js
@@ -228,3 +228,42 @@ obj.subscribe('nameChange', function(arg){
 });
  
 obj.publish('nameChange', 'john'); //sam, john
+
+
+/****************************************************
+                Beware Anonymous Functions
+****************************************************/
+// BAD
+$( document ).ready(function() {
+ 
+    $( "#magic" ).click(function( event ) {
+        $( "#yayeffects" ).slideUp(function() {
+            // ...
+        });
+    });
+ 
+    $( "#happiness" ).load( url + " #unicorns", function() {
+        // ...
+    });
+ 
+});
+ 
+// BETTER
+var PI = {
+ 
+    onReady: function() {
+        $( "#magic" ).click( PI.candyMtn );
+        $( "#happiness" ).load( PI.url + " #unicorns", PI.unicornCb );
+    },
+ 
+    candyMtn: function( event ) {
+        $( "#yayeffects" ).slideUp( PI.slideCb );
+    },
+ 
+    slideCb: function() { ... },
+ 
+    unicornCb: function() { ... }
+ 
+};
+ 
+$( document ).ready( PI.onReady );
